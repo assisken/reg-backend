0b35c965263f6993d001c920afa1d75ff2e84645
assisken <assisken@yandex.ru>
2018-05-31 17:25:28 +0300

    Filter and sorting users in admin.

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('sub', 'family_name', 'given_name', 'middle_name', 'email', 'email_verified',)
+    ordering = ('sub',)
+    list_filter = ('email_verified',)
